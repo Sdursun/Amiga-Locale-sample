cdcaf422db378ef76da4e2f3004cd546f2694941
Serkan DURSUN <55033030+Sdursun@users.noreply.github.com>
2023-11-28 22:44:13 +0300

    Add files via upload

---
+++ locale.h
@@ -0,0 +1,27 @@
+
+/****************************************************************
+
+   This file was created automatically by `FlexCat 2.18'
+   from "hello.cd".
+
+   Do NOT edit by hand!
+
+****************************************************************/
+
+#ifndef hello_CAT_H
+#define hello_CAT_H
+
+
+#ifndef EXEC_TYPES_H
+#include <exec/types.h>
+#endif  /*  !EXEC_TYPES_H           */
+
+
+/*  Prototypes  */
+extern STRPTR GetString(APTR);
+
+/*  Definitions */
+extern const APTR _msgContinue;
+#define msgContinue ((APTR) &_msgContinue)
+
+#endif /*   !hello_CAT_H  */
